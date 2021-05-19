c1eb093f41351ab32f71cdc4d264f935c4fc5c46
Brayan  Blas <brayan.blas.egoavil30@gmail.com>
2021-05-19 18:51:02 -0500

    first commit

---
+++ src/main/java/com/example/demo/VetRepository.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface VetRepository  extends CrudRepository<Veterinario,Long>{
+	List<Veterinario> findByName(String name);
+	List<Veterinario> findByLast(String last_name);
+}
